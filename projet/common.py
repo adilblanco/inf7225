b400fc4f99cb86c650adc7df93602ead24de32bb
Adil Labiad <adillabiad@gmai.com>
2023-10-19 13:12:33 -0400

    refactor

--- projet/common.py
+++ projet/common.py
@@ -11,6 +11,12 @@ def process_data(url):
     return df
 
 
+def prepare_data(gdf):
+    gdf.columns = gdf.columns.str.lower()
+    gdf_mtm8 = gdf.to_crs(epsg=MTM8)
+    return gdf_mtm8
+
+
 def create_geodataframe(df):
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude)).set_crs(f"epsg:{WGS84}")
     return gdf
