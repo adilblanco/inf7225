240d8ce4340f3715e58b0b7279b04691c7b5d11a
Adil Labiad <adillabiad@gmail.com>
2023-10-21 23:31:49 -0400

    create 'cycling-network-img' docker image

---
+++ projet/cycling-network-img/common.py
@@ -0,0 +1,26 @@
+import requests
+import geopandas as gpd
+
+WGS84 = 4326
+MTM8 = 32188
+
+
+def prepare_data(gdf):
+    gdf.columns = gdf.columns.str.lower()
+    gdf_mtm8 = gdf.to_crs(epsg=MTM8)
+    return gdf_mtm8
+
+
+def create_geodataframe(df):
+    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude)).set_crs(f"epsg:{WGS84}")
+    return gdf
+
+
+def process_request(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def json_items_to_geodataframe(http_response):
+    return gpd.GeoDataFrame().from_features(http_response).set_crs(f"epsg:{WGS84}")
